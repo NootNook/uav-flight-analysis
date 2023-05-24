b74018a49539b2f1c0371b072c89d7d92ccf73c4
NootNook <67638224+NootNook@users.noreply.github.com>
2023-05-24 09:51:00 +0200

    feat: dockerizing frontend and backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react({ include: '**/*.tsx' }), vanillaExtractPlugin()],
     server: {
+        host: true,
+        port: 5173,
         watch: {
             usePolling: true,
         },
